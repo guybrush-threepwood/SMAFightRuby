f9bf70d60c55d1bba0bf8ee9c6fce7cebc66a52f
Maximilien Noal <mnoal@epsi.fr>
2012-02-11 19:55:06 +0100

    BotHome translation from ActionScript (not tested)
    Signed-off-by: Maximilien Noal <mnoal@epsi.fr>

---
+++ MASWithTwoNests/bot_home.rb
@@ -0,0 +1,25 @@
+$LOAD_PATH << '../'
+require 'lib/point'
+include Lib
+$LOAD_PATH << './'
+require 'agent'
+require 'world'
+require 'rubygame'
+
+module MASWithTwoNests
+  class BotHome < Agent
+		attr_reader :team_id
+		def initialize(world, team_id, color)
+			super(world)
+			@team_id = team_id
+			@color = color
+			@count_text = ""
+			@image = Rubygame::Surface.new([World::HOME_RADIUS * 2, World::HOME_RADIUS * 2])
+			@rect = @image.make_rect
+			@image.draw_circle_s(@rect.center, Rubygame::Color::ColorRGB.new([0.1, 0.1, 0.1, 1]))
+		end
+
+
+	end
+end
+ 

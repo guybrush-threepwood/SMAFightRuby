423137aea55e6c3300c3c6e8e8360cca8816c9cc
Maximilien Noal <mnoal@epsi.fr>
2012-07-26 23:28:39 +0200

    CustomBot class
    Signed-off-by: Maximilien Noal <mnoal@epsi.fr>

---
+++ CustomTeam/custom_bot.rb
@@ -0,0 +1,25 @@
+$LOAD_PATH << '../'
+require 'MASWithTwoNests/Bot'
+
+module CustomTeam
+	class CustomBot < Bot
+		def initialize
+			super
+		end
+
+		def init_expert_system
+			super
+		end
+
+		def update_facts
+			super
+		end
+
+		def act
+			super
+		end
+
+		def on_collide(agent)
+			super
+		end
+end

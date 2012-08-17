7d6c712be745e2920276d1fd59045e384dd0d689
Maximilien Noal <mnoal@epsi.fr>
2012-08-17 19:58:27 +0200

    More SOLID ExpertSystem and FactBase, fixed is_rule_valid in the process
    Signed-off-by: Maximilien Noal <mnoal@epsi.fr>

--- ExpertSystem/expert_system.rb
+++ ExpertSystem/expert_system.rb
@@ -7,7 +7,6 @@ require 'fact'
 module ExpertSystem
   class ExpertSystem
     attr_reader :fact_base
-    attr_reader :rule_base
     attr_reader :inferred_facts
     def initialize
       @fact_base = FactBase.new
@@ -15,38 +14,40 @@ module ExpertSystem
       @inferred_facts = []
     end
 
-    def add_fact(fact)
-      @fact_base.facts_values.store(fact.to_sym, false)
-    end
-
-    def set_fact_value(fact, value)
-      @fact_base.facts_values[fact.to_sym] = value if @fact_base.has_fact(fact)
-    end
-
-    def get_fact_value(fact)
-      return @fact_base.facts_values[fact.to_sym] if @fact_base.has_fact(fact)
-    end
-
-    def reset_facts
-      @fact_base.facts_values.clear
-    end
-
     def add_rule(rule)
-      add_fact(rule) if not @fact_base.has_fact(rule)
+      @fact_base.add_fact(rule) if not @fact_base.has_fact(rule)
 
       rule.premises.each do |p|
-        add_fact(p) if not @fact_base.has_fact(p)
+        @fact_base.add_fact(p) if not @fact_base.has_fact(p)
       end
+
       @rule_base.add_rule(rule)
     end
 
+    def infer
+      clear_inferred_facts
+
+      begin
+        valid_rule = get_valid_rule
+				if valid_rule
+					@fact_base.set_fact_value(valid_rule.goal, true)
+					@inferred_facts << valid_rule.goal
+				end
+      end while valid_rule != nil
+    end
+
+		def set_fact_value(fact, value)
+			@fact_base.set_fact_value(fact, value)
+		end
+
+		private
+
     def is_rule_valid(rule)
-      return false if get_fact_value(rule.goal)
+      return false if @fact_base.get_fact_value(rule.goal)
 
       rule.premises.each do |p|
-        return false if not get_fact_value(p)
+        return @fact_base.get_fact_value(p)
       end
-      return true
     end
 
     def get_valid_rule
@@ -58,18 +59,6 @@ module ExpertSystem
 
     def clear_inferred_facts
       @inferred_facts = []
-    end
-
-    def infer
-      clear_inferred_facts
-
-      begin
-        valid_rule = get_valid_rule
-				if valid_rule
-					set_fact_value(valid_rule.goal, true)
-					@inferred_facts << valid_rule.goal
-				end
-      end while valid_rule != nil
     end
   end
 end
--- ExpertSystem/fact_base.rb
+++ ExpertSystem/fact_base.rb
@@ -1,12 +1,27 @@
 module ExpertSystem
   class FactBase
-    attr_accessor :facts_values
     def initialize
       @facts_values = {}
     end
 
     def has_fact(fact)
+      @facts_values.keys.include?(fact.to_sym)
+    end
+
+    def add_fact(fact)
+      @facts_values.store(fact.to_sym, false)
+    end
+
+    def set_fact_value(fact, value)
+      @facts_values[fact.to_sym] = value
+    end
+
+    def get_fact_value(fact)
       @facts_values[fact.to_sym]
+    end
+
+    def reset_facts
+      @facts_values.clear
     end
   end
 end

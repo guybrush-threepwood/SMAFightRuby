7d158c80bdbecc026308312fc23f98f3a9a65f7d
Maximilien Noal <mnoal@epsi.fr>
2012-08-22 21:04:03 +0200

    BotHome : redraw sprite when Resource amount changes
    The sprite can be bigger only if "home getting bigger" is checked
    
    Signed-off-by: Maximilien Noal <mnoal@epsi.fr>

--- MASWithTwoNests/bot_home.rb
+++ MASWithTwoNests/bot_home.rb
@@ -16,18 +16,36 @@ module MASWithTwoNests
 			@resource_count = 0
 			@team_id = team_id
 			@color = color
-			@image = Rubygame::Surface.new([World::HOME_RADIUS * 2, World::HOME_RADIUS * 2])
-			@rect = @image.make_rect
-      @image.set_colorkey([0, 0, 0])
-			@image.draw_circle_s(@rect.center, World::HOME_RADIUS, Rubygame::Color::ColorRGB.new([0.22745, 0.22745, 0.22745, 1]))
-			@rect.center = [rand * World::HEIGHT, rand * World::HEIGHT]
+			@radius = nil
+			@center = [rand * World::HEIGHT, rand * World::HEIGHT]
+			draw_sprite(world)
 		end
 
 		def update(tick, world)
+			draw_sprite(world)
 		end
 
 		def add_resource
 			@resource_count += 1
+		end
+
+		private
+
+		def draw_sprite(world)
+			new_radius = nil
+			if world.home_getting_bigger
+				new_radius = World::HOME_RADIUS + @resource_count
+			else
+				new_radius = World::HOME_RADIUS
+			end
+			if new_radius != @radius or @radius == nil
+				@radius = new_radius
+				@image = Rubygame::Surface.new([@radius * 2, @radius * 2])
+				@rect = @image.make_rect
+				@image.set_colorkey([0, 0, 0])
+				@image.draw_circle_s(@rect.center, @radius, @color)
+				@rect.center = @center
+			end
 		end
 	end
 end
